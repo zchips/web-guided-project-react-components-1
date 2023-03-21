6f80cce3961a5c05670c241eab68760a41bceb85
zchips <zchips@gmail.com>
2023-03-21 16:58:14 -0600

    finished gp

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -10,10 +10,56 @@
 */
 
 
-import React from "react";
+import React, {useState} from "react";
 
 function Playground(props){
-  return <h1>Playground</h1>
-}
+
+  const [count, setCount] = useState(0)
+  const [spinnerOn, setSpinnerOn]=useState(false)
+  const {gesture, setGesture} =useState('scissors')
+
+
+  // setCount(count +500)
+
+  if(spinnerOn){
+    return(
+      <div className="container">
+        <h3>The spinner is {spinnerOn ? "On": "Off"}</h3>
+        <button onClick={evt => setSpinnerOn(false)}>turn spinner off</button>
+      </div>
+    )
+  }
+
+
+
+  return(
+    <div>
+      <p>{count}</p>
+    <button onClick ={()=> setCount(count + 1)}>Incrementing or adding by one
+    </button>
+    <button onClick={()=> setCount(count - 1)}>Subtracting 1</button>
+    
+    
+    
+    <h3>the spinner is {spinnerOn? "On" : "Off"}</h3>
+    <button onClick={()=> setSpinnerOn(spinnerOn)}>toggle spinner</button>
+
+
+
+    <h3>The current gesture is {gesture}</h3>
+    <button onClick={()=> setGesture('scissors')}>pick scissors</button>
+    <button onClick={()=> setGesture('rock')}> pick rock</button>
+    <button onClick={()=> setGesture('paper')}> pick paper</button>
+    
+    
+    </div>
+
+
+
+  )
+    
+
+
+  }
 
 export default Playground
